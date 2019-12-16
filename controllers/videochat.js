3c3481a42766f6390d7da27d481d42dbead5f451
rashalismath2 <rashalismath@gmail.com>
2019-12-16 20:45:00 +0530

    video chat added with cons user

--- controllers/videochat.js
+++ controllers/videochat.js
@@ -15,6 +15,33 @@ module.exports.videochatinit=function(req,res){
            message:"video init sent"
        });
 }
+
+module.exports.videochatinitwithcons=function(req,res){
+    console.log("3")
+    var data={
+        from:req.body.from,
+        name:req.userFirstName+" "+req.userLastName,
+        to:req.to
+    }
+       pusher.trigger('private-sdp-client-cons-'+req.body.to, 'video-chat-init-cons',data);
+       res.status(200).json({
+           message:"video init sent"
+       });
+}
+
+module.exports.videochatinitwithuser=function(req,res){
+    var data={
+        from:req.body.from,
+        name:req.userFirstName+" "+req.userLastName,
+        to:req.to
+    }
+       pusher.trigger('private-sdp-client-user-'+req.body.to, 'video-chat-init-user',data);
+       res.status(200).json({
+           message:"video init sent"
+       });
+}
+
+
 module.exports.sendicecandidate=function(req,res){
     var data={
         from:req.body.from,
@@ -25,6 +52,28 @@ module.exports.sendicecandidate=function(req,res){
            message:"ice-candidate sent"
        });
 }
+module.exports.sendicecandidatetocons=function(req,res){
+    var data={
+        from:req.body.from,
+        payload:req.body.payload
+    }
+       pusher.trigger('private-sdp-client-cons-'+req.body.to, 'get-ice-candidate-cons',data);
+       res.status(200).json({
+           message:"ice-candidate sent"
+       });
+}
+module.exports.sendicecandidatetouser=function(req,res){
+    var data={
+        from:req.body.from,
+        payload:req.body.payload
+    }
+       pusher.trigger('private-sdp-client-user-'+req.body.to, 'get-ice-candidate-user',data);
+       res.status(200).json({
+           message:"ice-candidate sent"
+       });
+}
+
+
 
 module.exports.videocallgotdisconnected=function(req,res){
        pusher.trigger('sdp-client-'+req.body.with, 'video-call-distrupted',req.body.state);
@@ -32,8 +81,19 @@ module.exports.videocallgotdisconnected=function(req,res){
            message:" video call distrupted sent"
        });
 }
+module.exports.videocallgotdisconnectedwithcons=function(req,res){
+       pusher.trigger('private-cons-sdp-client-'+req.body.with, 'video-call-distrupted-cons',req.body.state);
+       res.status(200).json({
+           message:" video call distrupted sent"
+       });
+}
+module.exports.videocallgotdisconnectedwithuser=function(req,res){
+       pusher.trigger('private-user-sdp-client-'+req.body.with, 'video-call-distrupted-user',req.body.state);
+       res.status(200).json({
+           message:" video call distrupted sent"
+       });
+}
 module.exports.getinitoffer=function(req,res){
- 
     twillio.create.then(t=>{
         var data={
             requestFrom:req.body.requestFrom,
@@ -46,7 +106,32 @@ module.exports.getinitoffer=function(req,res){
             message:"init offer request init sent"
         });
     })
-
+}
+module.exports.getinitofferfromuser=function(req,res){
+    twillio.create.then(t=>{
+        var data={
+            requestFrom:req.body.requestFrom,
+            name:req.userFirstName+" "+req.userLastName,
+            ICE_SERVERS:t.iceServers
+        }
+        pusher.trigger('private-sdp-client-user-'+req.body.requestTo, 'get-init-offer-user',data);
+        res.status(200).json({
+            message:"init offer request init sent"
+        });
+    })
+}
+module.exports.getinitofferfromcons=function(req,res){
+    twillio.create.then(t=>{
+        var data={
+            requestFrom:req.body.requestFrom,
+            name:req.userFirstName+" "+req.userLastName,
+            ICE_SERVERS:t.iceServers
+        }
+        pusher.trigger('private-sdp-client-cons-'+req.body.requestTo, 'get-init-offer-cons',data);
+        res.status(200).json({
+            message:"init offer request init sent"
+        });
+    })
 }
 
 module.exports.sendinitoffer=function(req,res){
@@ -67,6 +152,56 @@ module.exports.sendinitoffer=function(req,res){
   
 }
 
+module.exports.sendinitofferforcons=function(req,res){  
+    twillio.create.then(t=>{
+        var data={
+            offer:req.body.sdp,
+            fromName:req.userFirstName+" "+req.userLastName,
+            fromId:req.userId,
+            ICE_SERVERS:t.iceServers
+        }
+      
+        pusher.trigger('private-sdp-client-cons-'+req.body.sendTo, 'offer-sdp-cons',data);
+        res.status(200).json({
+            message:"sdp sent"
+        });
+    })
+  
+}
+module.exports.sendinitofferforuser=function(req,res){  
+    twillio.create.then(t=>{
+        var data={
+            offer:req.body.sdp,
+            fromName:req.userFirstName+" "+req.userLastName,
+            fromId:req.userId,
+            ICE_SERVERS:t.iceServers
+        }
+      
+        pusher.trigger('private-sdp-client-user-'+req.body.sendTo, 'offer-sdp-user',data);
+        res.status(200).json({
+            message:"sdp sent"
+        });
+    })
+  
+}
+// module.exports.videochatinitwithcons=function(req,res){
+
+//     twillio.create.then(t=>{
+//         var data={
+//             offer:req.body.sdp,
+//             fromName:req.userFirstName+" "+req.userLastName,
+//             fromId:req.userId,
+//             ICE_SERVERS:t.iceServers
+//         }
+      
+//         pusher.trigger('private-sdp-client-cons-'+req.body.sendTo, 'offer-sdp-cons',data);
+//         res.status(200).json({
+//             message:"sdp sent"
+//         });
+//     })
+  
+// }
+
 module.exports.declinedinitoffer=function(req,res){
     var data={
         fromName:req.body.declinedBy,
@@ -85,6 +220,27 @@ module.exports.sendAnswerSDP=function(req,res){
         answerSDPFrom:req.body.answerSDPFrom
     }
        pusher.trigger('sdp-client-'+req.body.answerSDPTo, 'get-answer-sdp',data);
+       res.status(200).json({
+           message:"answer sdp has been sent"
+       });
+}
+
+module.exports.sendAnswerSDPForUser=function(req,res){
+    var data={
+        answerSDP:req.body.answerSDP,
+        answerSDPFrom:req.body.answerSDPFrom
+    }
+       pusher.trigger('private-sdp-client-user-'+req.body.answerSDPTo, 'get-answer-sdp-user',data);
+       res.status(200).json({
+           message:"answer sdp has been sent"
+       });
+}
+module.exports.sendAnswerSDPForCons=function(req,res){
+    var data={
+        answerSDP:req.body.answerSDP,
+        answerSDPFrom:req.body.answerSDPFrom
+    }
+       pusher.trigger('private-sdp-client-cons-'+req.body.answerSDPTo, 'get-answer-sdp-cons',data);
        res.status(200).json({
            message:"answer sdp has been sent"
        });
--- routes/videochat.js
+++ routes/videochat.js
@@ -4,13 +4,35 @@ const videochatController=require("../controllers/videochat");
 
 
 router.post("/sendinitoffer",videochatController.sendinitoffer);
+router.post("/sendinitofferforcons",videochatController.sendinitofferforcons);
+router.post("/sendinitofferforuser",videochatController.sendinitofferforuser);
+
+
 router.post("/declinedinitoffer",videochatController.declinedinitoffer);
+
 router.post("/sendAnswerSDP",videochatController.sendAnswerSDP);
+router.post("/sendAnswerSDPForUser",videochatController.sendAnswerSDPForUser);
+router.post("/sendAnswerSDPForCons",videochatController.sendAnswerSDPForCons);
+
 
 router.post("/videochatinit",videochatController.videochatinit);
+router.post("/videochatinitwithcons",videochatController.videochatinitwithcons);
+router.post("/videochatinitwithuser",videochatController.videochatinitwithuser);
+
+
 router.post("/getinitoffer",videochatController.getinitoffer);
+router.post("/getinitofferfromuser",videochatController.getinitofferfromuser);
+router.post("/getinitofferfromcons",videochatController.getinitofferfromcons);
+
+
 router.post("/sendicecandidate",videochatController.sendicecandidate);
+router.post("/sendicecandidatetocons",videochatController.sendicecandidatetocons);
+router.post("/sendicecandidatetouser",videochatController.sendicecandidatetouser);
+
+
 router.post("/videocallgotdisconnected",videochatController.videocallgotdisconnected);
+router.post("/videocallgotdisconnectedwithcons",videochatController.videocallgotdisconnectedwithcons);
+router.post("/videocallgotdisconnectedwithuser",videochatController.videocallgotdisconnectedwithuser);
 
 
 
